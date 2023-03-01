2e6f143833824f1fa84b267ba939f33d9971ca1d
Jeremiah Quill <jcq5010@gmail.com>
2023-02-28 22:25:35 -0500

    add google auth sign in

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,42 @@
+import { useSession } from "next-auth/react";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
   return (
     <div className="bg-slate-100 min-h-[100vh] text-slate-800 flex flex-col">
       <header className="font-extrabold p-4 flex items-center">
-        <h1 className="text-2xl">Recipes</h1>
+        <h1 className="text-2xl">
+          <a href="/">Recipes</a>
+        </h1>
         <nav className="ml-auto">
-          <ul className="flex gap-8">
+          <ul className="flex gap-8 items-center">
             <li>
               <a href="#">About</a>
             </li>
             <li>
               <a href="#">Browse</a>
             </li>
-            <li>
-              <a href="#">Sign In</a>
-            </li>
-            <li>
-              <a href="#">Sign up</a>
-            </li>
+            {!session ? (
+              <li>
+                <a href={`/api/auth/signin`}>Sign In</a>
+              </li>
+            ) : (
+              <>
+                <div className="flex gap-2 items-center border-2 border-slate-800 px-2 py-1 rounded">
+                  <div>{session.user?.email}</div>
+                  <img
+                    src={session.user?.image}
+                    alt="profile picture"
+                    className="w-10 rounded-full border-2 border-slate-800"
+                  />
+                </div>
+                <li>
+                  <a href={`/api/auth/signout`}>Sign Out</a>
+                </li>
+              </>
+            )}
           </ul>
         </nav>
       </header>
